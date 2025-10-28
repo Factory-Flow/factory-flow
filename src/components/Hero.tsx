63deed9346bf4b40452ed6a4804d426f30f9b182
adrianiannantuono <aiannantuono@me.com>
2025-10-28 17:05:41 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,6 @@
 
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
